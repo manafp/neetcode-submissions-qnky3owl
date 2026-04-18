ad3188cbc0e7949e147dd3275ec48c3943dbf8b1
draxFrk <abdulmanaf469421@gmail.com>
2026-04-18 22:05:05 +0530

    Add: longest-repeating-substring-with-replacement - submission-9

---
+++ Data Structures & Algorithms/longest-repeating-substring-with-replacement/submission-9.cs
@@ -0,0 +1,31 @@
+public class Solution {
+    public int CharacterReplacement(string s, int k) {
+        
+        int res = 0;
+        var charSet  = new Dictionary<char,int>();
+
+           int l = 0;int maxF = 0;
+           for(int r = 0;r < s.Length;r++)
+           {
+             if(charSet.ContainsKey(s[r]))
+             {
+               charSet[s[r]]++;
+             }
+             else
+             {
+              charSet[s[r]] = 1;
+             }
+             maxF = Math.Max(maxF,charSet[s[r]]);
+             
+             while((r-l+1) - maxF > k)
+             {
+              charSet[s[l]]--;
+              l++;
+             }
+              res = Math.Max(r-l+1,res);
+
+           }
+        return res;
+    }
+}
+
