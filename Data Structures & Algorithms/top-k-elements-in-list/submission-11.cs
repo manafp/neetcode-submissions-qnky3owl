cf43a94fd62bfc3821df647ef07f3049b3b8344d
draxFrk <abdulmanaf469421@gmail.com>
2026-08-19 22:51:21 +0530

    Add: top-k-elements-in-list - submission-11

---
+++ Data Structures & Algorithms/top-k-elements-in-list/submission-11.cs
@@ -0,0 +1,41 @@
+public class Solution {
+    public int[] TopKFrequent(int[] nums, int k) {
+       var count = new Dictionary<int,int>();
+       var freq = new List<int>[nums.Length + 1];
+
+       for(int i=0;i<freq.Length;i++)
+       {
+         freq[i] = new List<int>();
+       }
+
+       foreach(var num in nums)
+       {
+         if(count.ContainsKey(num))
+         {
+            count[num]++;
+         }
+         else
+         {
+            count[num] = 1;
+         }
+       }
+
+       foreach(var entry in count)
+       {
+          freq[entry.Value].Add(entry.Key);
+       }
+       
+       var res = new int[k];
+       var index = 0;
+       for(int i = freq.Length - 1;i>=0 && index<k; i--)
+       {
+         foreach(var entry in freq[i])
+         {
+            res[index++] = entry;
+            if(index > k) break;
+         }
+       }
+
+       return res;
+    }
+}
