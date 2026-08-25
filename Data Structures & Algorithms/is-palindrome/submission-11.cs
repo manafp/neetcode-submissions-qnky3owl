c02007a42c614ecde3453a15b536dcee9abece12
draxFrk <abdulmanaf469421@gmail.com>
2026-08-25 21:39:13 +0530

    Add: is-palindrome - submission-11

---
+++ Data Structures & Algorithms/is-palindrome/submission-11.cs
@@ -0,0 +1,26 @@
+public class Solution {
+    public bool IsPalindrome(string s) {
+        if(s.Length == 1)
+          return true;
+        int i = 0;
+        int j = s.Length -1;
+
+        while(i <= j)
+        {
+            while(!char.IsLetter(s[i]) && !char.IsDigit(s[i]) && i <= j)
+            {
+                i++;               
+            }
+            while(!char.IsLetter(s[j]) && !char.IsDigit(s[j]) && i<= j)
+            {
+                j--;
+            }
+            if(char.ToLower(s[i]) != char.ToLower(s[j]))
+              return false;
+
+            i++;
+            j--;
+        }
+        return true;
+    }
+}
