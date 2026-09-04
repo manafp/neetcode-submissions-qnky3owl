59a400eb7d68653166e83094dbdc08aef868ef87
draxFrk <abdulmanaf469421@gmail.com>
2026-09-04 23:21:37 +0530

    Add: validate-parentheses - submission-8

---
+++ Data Structures & Algorithms/validate-parentheses/submission-8.cs
@@ -0,0 +1,26 @@
+public class Solution {
+    public bool IsValid(string s) {
+
+        var dic = new Dictionary<char,char>()
+        {
+            {')','('},
+            {'}','{'},
+            {']','['}
+        };
+        var stack = new Stack<char>();
+        for(int i = 0;i<s.Length;i++)
+        {
+            if(!dic.ContainsKey(s[i]))
+            {
+                stack.Push(s[i]);
+            }
+            else
+            {
+                var pop = stack.Pop();
+                if(pop != dic[s[i]])
+                  return false;
+            }
+        }
+        return stack.Count == 0;
+    }
+}
