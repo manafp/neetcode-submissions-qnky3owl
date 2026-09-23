34c48609d96531e822505e208dd7d5b7a76c83d4
draxFrk <abdulmanaf469421@gmail.com>
2026-09-23 23:01:03 +0530

    Add: search-2d-matrix - submission-4

---
+++ Data Structures & Algorithms/search-2d-matrix/submission-4.cs
@@ -0,0 +1,33 @@
+public class Solution {
+    public bool SearchMatrix(int[][] matrix, int target) {
+         int l = 0;
+         int r = matrix.Length - 1;
+         
+         while(l <= r)
+         {
+            int mid = (l + r) / 2;
+
+            if(matrix[mid][0] == target)return true;
+
+            if(target > matrix[mid][0] && target < matrix[mid + 1][0])
+            {
+                 l = 0;
+                 r = matrix[mid].Length - 1;
+
+                while(l <= r)
+                {
+                   int imid = (l + r) / 2;
+
+                   if(matrix[mid][imid] == target) return true;
+                   if(matrix[mid][imid] < target) l = mid + 1;
+                   else r = mid -1;
+                }
+                break;
+            }
+            if(target < matrix[mid][0]) r = mid - 1;
+            else l = mid + 1;
+         }  
+
+         return false;
+    }
+}
